0a627e8258ee55244a2583de5f0b26d6907fd33b
root <root@slack.jjharden.com>
2017-03-14 23:40:37 +0000

    fixed comma errors

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 module.exports = {
-	api_ai_key: "{insert_api_ai_key_here}"
-	slack_bot_key: "{insert_slack_bot_key_here}"
+	api_ai_key: "{insert_api_ai_key_here}",
+	slack_bot_key: "{insert_slack_bot_key_here}",
 	meme_generator: {
 		username: "{insert_meme_generator_username_here}",
 		password: "{insert_meme_generator_password_here}"
