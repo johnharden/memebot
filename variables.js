e0f18009c4ca739c491eba19f6d997f3c23bd7c6
root <root@vultr.guest>
2017-07-11 18:03:56 +0000

    Whoops

--- variables.js
+++ variables.js
@@ -1,8 +1,8 @@
 module.exports = {
-	api_ai_key: "9744884a92de41aebd676783eaefa28f",
-	slack_bot_key: "xoxb-210491653552-qzGMjw3124tdfxN5301xNJuE",
-	meme_generator: {
-		username: "johnharden0",
-		password: "password1234!"
-	}
+        api_ai_key: "",
+        slack_bot_key: "",
+        meme_generator: {
+                username: "",
+                password: ""
+        }
 };
